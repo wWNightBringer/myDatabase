3194084de21d03909f604efa1154d7dbd6a05254
andre <andrew@simplex.tv>
2022-07-23 16:32:58 +0300

    Initial commit

---
+++ src/main/java/com/example/mydatabase/MyDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.mydatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mydatabase/MyDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mydatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
